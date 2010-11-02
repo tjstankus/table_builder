a80ecd72695ee8c90fba89b1afbce75dec1d6849
zaiste <oh@zaiste.net>
2010-11-02 14:33:15 +0100

    schedule helper added

--- lib/table_builder.rb
+++ lib/table_builder.rb
@@ -1,5 +1,7 @@
 require(File.expand_path(File.join(File.dirname(__FILE__), 'table_builder', 'table_builder.rb')))
 require(File.expand_path(File.join(File.dirname(__FILE__), 'table_builder', 'calendar_helper.rb')))
+require(File.expand_path(File.join(File.dirname(__FILE__), 'table_builder', 'schedule_helper.rb')))
 
 ActionView::Base.send :include, TableHelper
 ActionView::Base.send :include, CalendarHelper
+ActionView::Base.send :include, ScheduleHelper
